77a3a6452563431590a75c921ab1aebe76462951
Alan Cleyton <alancleyton67@gmail.com>
2024-02-01 18:03:29 -0300

    basics: comments in python

---
+++ basics/comments.py
@@ -0,0 +1,14 @@
+# this is the first comment
+print('Hello World!') # and this is the second comment
+                      # ... and now a third!
+
+""" this is a docstring """
+
+''' this also is a docstring '''
+
+"""
+this is a multiline docstring:
+
+1 - this do something cool
+2 - this do something amazing
+"""
