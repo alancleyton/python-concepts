846e99bc8420b1dd3f37a8670dadf0794a109e8a
Alan Cleyton <alancleyton67@gmail.com>
2024-02-06 18:51:24 -0300

    data types: strings

---
+++ data_types/strings.py
@@ -0,0 +1,30 @@
+'''
+Everything is an object in Python, data types are classes and variables are instances (objects) of these classes.
+
+Strings are sequences of character data. The string type in Python is called str
+str -> string -> text
+'''
+
+# single-quotes string
+print('I am a single-quote string.')
+
+# double-quote string
+print("I am a double-quote string.")
+
+'''
+In Python strings, the backslash "\" is a special character, also called the "escape" character. It is used in representing certain whitespace characters.
+'''
+print("I am \"Groot\"")
+print('It\'s raining')
+
+'''
+Strings with the other type. If a string is to contain a single quote, delimit it with double quotes and vice versa:
+'''
+print("This string contains a single quote (') character.")
+print('This string contains a double quote (") character.')
+
+# Other escape characters used in Python:
+print('foo\tbar') # Tab
+print('foo\bbar') # Backspace
+print('Hello \nworld!') # New Line
+print('Sinopse:\fOunce upon a time...') # Form Feed	
