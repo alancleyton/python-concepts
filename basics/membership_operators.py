871394840f3ed106d964b0cb6cc45cf840c41b7c
Alan Cleyton <alancleyton67@gmail.com>
2024-03-06 16:17:31 -0300

    basics: membership operators

---
+++ basics/membership_operators.py
@@ -0,0 +1,12 @@
+'''
+In Python, the membership operators allow you to check the presence or absence in an object.
+'''
+
+# IN returns True if a sequence with the specified value is present in the object
+name = 'John'
+print('o' in name) # returns True
+print('a' in name) # returns False
+
+# NO IN returns True if a sequence with the specified value is not present in the object
+print('a' not in name) # returns True
+print('o' not in name) # returns False
