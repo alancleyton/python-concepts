8cc044408d9938828126c39108155ef9151bc74c
Alan Cleyton <alancleyton67@gmail.com>
2024-03-14 15:36:46 -0300

    basics: variables and constants

---
+++ basics/variables.py
@@ -0,0 +1,25 @@
+# In Python we use the assignment operator = to assign a value to a variable.
+name = 'John Doe'
+
+# The value and type can be changed after assignment
+name = 34
+
+# Assigning multiple variables
+first_name, last_name, age = 'John', 'John', 27
+print(first_name) # John
+print(last_name) # John
+print(age) # 27
+
+# Assigning the same value to multiple variables
+x = y = z = 35
+print(x) # 35
+print(y) # 35
+print(z) # 35
+
+'''
+Constants in Python is a type of variable that holds values that cannot be changed.
+There's no keyword as in other languages to create a constant variable, usually constants is declare it in upper case.
+'''
+PI = 3.14
+FOO = 0
+URL = 'www.google.com'
