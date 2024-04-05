c0d93aba5dd65ddfb7ad3228df34d50e50634aa0
Alan Cleyton <alancleyton67@gmail.com>
2024-04-05 12:00:03 -0300

    basics: functions

---
+++ basics/functions.py
@@ -0,0 +1,50 @@
+'''
+Functions in Python is a block of code that runs a specific task.
+Functions are used to increase code readability and reuse, avoiding writing the same code again and again.
+'''
+
+# functions are defined with the keyword def, followed by the function name enclosed in parentheses ()
+def my_awesome_function():
+  print('Hello my awesome Function!')
+
+# use the function name enclosed in parentheses to call a function
+my_awesome_function()
+
+# optional inputs can be passed into functions as arguments
+def greetings(name):
+  print(f'Hello, {name}')
+
+greetings('John')
+
+'''
+In Python, arguments can be passed to a function in two ways: positional arguments or keyword arguments:
+- Positional arguments (also called required arguments) are passed to a function based on its position in the list of parameters inside the parentheses.
+- Keyword arguments are passed to a function by specifying the parameter name and its corresponding value.
+'''
+
+def total_price(qty, item, price):
+  print(f'{qty} {item} cost ${price:.2f}')
+
+# calling the function with positional arguments
+total_price(5, 'bananas', 4.64)
+
+# calling the function with keyword arguments
+total_price(item='apples', qty=10, price=23.84)
+
+# function can be called with both positional and keyword arguments
+total_price(2, 'notebooks', price=33.54)
+
+# when positional and keyword arguments are passed to a function, all the positional arguments must come first inside the parentheses
+# total_price(1, item='tablet', 629.99) # SyntaxError: positional argument follows keyword argument
+
+# function parameters can be defined with a default value
+def user(firstname, lastname, age=24):
+  print(f"My name is {firstname} {lastname} and i'm {age} years old")
+
+user('john', 'Doe')
+
+# default parameter value can be changed on function call.
+user('Tim', 'Martin', 33)
+
+# when a parameter is defined with a default value, all the required parameters must come first inside the parentheses
+# def user(firstname, lastname, age=24, id): # SyntaxError: non-default argument follows default argument#
