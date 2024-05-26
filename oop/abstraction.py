0c34e41b3f80553ab90bbeb3ab0035b2b1ee8ab5
Alan Cleyton <alancleyton67@gmail.com>
2024-05-26 10:30:58 -0300

    oop: abstraction

---
+++ oop/abstraction.py
@@ -0,0 +1,4 @@
+'''
+Abstraction is one of the core concepts of OOPs, it used to create a superclass that encapsulates shared attributes and methods for subclasses.
+- Methods in the abstract class can serve as templates to be implemented by subclasses.
+'''
